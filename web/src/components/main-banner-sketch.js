f73be6be3477ab3278f339f3996e9566cd2bc78f
Jeeyoon <acetaire@naver.com>
2020-09-22 01:24:09 -0400

    added p5 sketch in main page

--- web/src/components/main-banner-sketch.js
+++ web/src/components/main-banner-sketch.js
@@ -1,33 +1,86 @@
-// Last Revised: 2020.09.20
-// Original Version: 2019.07.31
+// Last Revised: 2020.09.22
+
 export const sketch = (p) => {
   let canvas;
+  let width;
+  let height;
+
+  // p5 variables
   let position;
   let velocity;
+  let radius = 4;
+  let angle = 0;
+  let aVel = 0;
+  let aAccel = 0.005;
+
+  // update canvas size when resized
+  const updateSize = () => {
+    width = document.getElementById("wrap").clientWidth;
+    height = document.getElementById("wrap").clientHeight;
+    console.log("updated size to" + width);
+  };
+  window.onload = updateSize;
+  window.onresize = updateSize;
 
   p.setup = () => {
-    let canvas = p.createCanvas("50vw", 200);
-    position = p.createVector(100, 100);
-    velocity = p.createVector(2.5, 5);
-    // canvas.style("z-index", "-1");
-    canvas.style("width", "50vw");
-    canvas.style("margin", "auto");
+    updateSize();
+    let canvas = p.createCanvas(width, height);
+    position = p.createVector(p.random(width), p.random(height));
+    velocity = p.createVector(1.5, 2.4);
   };
 
   p.draw = () => {
-    p.background("yellow");
-    //   position.add(velocity);
+    p.resizeCanvas(width, height);
+    p.background(0);
 
-    //   if (position.x > p.width || position.x < 0) {
-    //     velocity.x = velocity.x * -1;
-    //   }
+    // update
 
-    //   if (position.y > p.height || position.y < 0) {
-    //     velocity.y = velocity.y * -1;
-    //   }
+    angle += aVel;
+    aVel += aAccel;
 
-    //   p.stroke(0);
-    //   p.fill(0);
-    //   p.ellipse(position.x, position.y, 16, 16);
+    position.add(velocity);
+
+    if (position.x > width || position.x < 0) {
+      velocity.x = velocity.x * -1;
+      aAccel = aAccel * -1;
+      aVel = 0;
+    }
+
+    if (position.y > height || position.y < 0) {
+      velocity.y = velocity.y * -1;
+      aAccel = aAccel * -1;
+      aVel = 0;
+    }
+
+    // draw
+    p.stroke(255);
+    p.fill(255);
+    p.ellipse(position.x, position.y, radius, radius);
+
+    p.push();
+    p.translate(position.x, position.y);
+    p.rotate(angle);
+    for (let i = 1; i < 48; i++) {
+      p.ellipse(2 * radius * p.sin(p.PI * i * 0.25), 2 * radius * p.cos(p.PI * i * 0.25), 0.8);
+    }
+    for (let i = 1; i < 24; i++) {
+      p.ellipse(4 * radius * p.sin(p.PI * i * 0.25), 4 * radius * p.cos(p.PI * i * 0.25), 0.4);
+    }
+    for (let i = 1; i < 12; i++) {
+      p.ellipse(4 * radius * p.sin(p.PI * i * 0.5), 4 * radius * p.cos(p.PI * i * 0.25), 0.1);
+    }
+    p.pop();
+
+    // // draw rings
+    // p.noFill();
+    // p.strokeWeight(0.25);
+    // p.ellipse(position.x, position.y, 4 * radius, 4 * radius * p.sin(p.frameCount / 100));
+    // p.ellipse(position.x, position.y, 4 * radius, 4 * radius * p.cos(4 + p.frameCount / 100) * 2);
+    // p.ellipse(
+    //   position.x,
+    //   position.y,
+    //   radius * p.cos(2 + p.frameCount / 100) * 4,
+    //   radius * p.sin(p.frameCount / 100) * 1.2
+    // );
   };
 };
--- web/src/components/main-banner.js
+++ web/src/components/main-banner.js
@@ -3,30 +3,16 @@ import { useP5 } from "@gen/react-use-p5";
 import { sketch } from "./main-banner-sketch.js";
 import styles from "./main-banner.css";
 
-function MainBanner({ headline }) {
+const MainBanner = ({ headline }) => {
+  const [bgSketch] = useP5(sketch);
+  // Canvas will be rendered as child of div
   return (
-    <div className="sketch-container">
+    <div className="wrapper" id="wrap" ref={bgSketch}>
       <div className="bg">
         <h1>{headline}</h1>
-        {/* <canvas></canvas>
-        <script src="./man-banner-sketch.js"></script> */}
-        {/* TODO: add main-banner-sketch.js */}
-        {/* todo: add category */}
       </div>
     </div>
   );
-}
-
-// const MainBanner = ({ headline }) => {
-//   const [bgSketch] = useP5(sketch);
-//   // Canvas will be rendered as child of div
-//   return (
-//     <div className="wrapper" ref={bgSketch}>
-//       <div className="bg">
-//         <h1>{headline}</h1>
-//       </div>
-//     </div>
-//   );
-// };
+};
 
 export default MainBanner;
