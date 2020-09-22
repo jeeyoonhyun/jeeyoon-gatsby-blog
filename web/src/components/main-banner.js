9883726508695baa3f7f9426a0e13f126dc7b97f
Jeeyoon <acetaire@naver.com>
2020-09-22 19:45:41 -0400

    rollback p5 part for updating blog

--- web/src/components/main-banner.js
+++ web/src/components/main-banner.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { useP5 } from "@gen/react-use-p5";
-import { sketch } from "./main-banner-sketch.js";
+// import Sketch from "react-p5";
 import styles from "./main-banner.css";
 
-const MainBanner = ({ headline }) => {
-  const [bgSketch] = useP5(sketch);
-  // Canvas will be rendered as child of div
+function MainBanner({ headline }) {
   return (
-    <div className="wrapper" id="wrap" ref={bgSketch}>
+    <div className="sketch-container">
       <div className="bg">
         <h1>{headline}</h1>
+        {/* <canvas></canvas>
+        <script src="./man-banner-sketch.js"></script> */}
+        {/* TODO: add main-banner-sketch.js */}
+        {/* todo: add category */}
       </div>
     </div>
   );
-};
+}
 
 export default MainBanner;
