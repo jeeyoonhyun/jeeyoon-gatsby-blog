7f62cfe9219851004b3fae086ee5ca5098d34952
Jeeyoon <acetaire@naver.com>
2020-10-11 02:06:01 -0400

    modify bodyportabletext

--- studio/schemas/objects/youtube.js
+++ studio/schemas/objects/youtube.js
@@ -3,27 +3,9 @@ import getYouTubeId from 'get-youtube-id'
 import YouTube from 'react-youtube'
 
 const Preview = ({ value }) => {
-  // const { url } = value
-  // const id = getYouTubeId(url)
-  // return <YouTube videoId={id} />
-  const id = getYouTubeId(value.url)
-  const url = `https://www.youtube.com/embed/${id}`
-
-  if (!id) {
-    return <div>Missing YouTube URL</div>
-  }
-
-  return (
-    <iframe
-      title="YouTube Preview"
-      width="560"
-      height="315"
-      src={url}
-      frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; clipboard-write; gyroscope; picture-in-picture"
-      allowFullScreen
-    />
-  )
+  const { url } = value
+  const id = getYouTubeId(url)
+  return <YouTube videoId={id} />
 }
 
 export default {
