49828888722926d60d32c3410e27f9b249b6102c
Jeeyoon <acetaire@naver.com>
2020-10-10 21:29:05 -0400

    add youtube embed in studio

--- studio/schemas/objects/schema.js
+++ studio/schemas/objects/schema.js
@@ -5,18 +5,17 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // document schemas
-import author from './documents/author'
-import category from './documents/category'
-import post from './documents/post'
-import siteSettings from './documents/siteSettings'
+import author from '../documents/author'
+import category from '../documents/category'
+import post from '../documents/post'
+import siteSettings from '../documents/siteSettings'
 
 // Object types
-import bodyPortableText from './objects/bodyPortableText'
-import bioPortableText from './objects/bioPortableText'
-import excerptPortableText from './objects/excerptPortableText'
-import mainImage from './objects/mainImage'
-import authorReference from './objects/authorReference'
-import youtube from './objects/youtube'
+import bodyPortableText from './bodyPortableText'
+import bioPortableText from './bioPortableText'
+import excerptPortableText from './excerptPortableText'
+import mainImage from './mainImage'
+import authorReference from './authorReference'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -35,8 +34,7 @@ export default createSchema({
     authorReference,
     bodyPortableText,
     bioPortableText,
-    excerptPortableText,
-    youtube
+    excerptPortableText
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
   ])
