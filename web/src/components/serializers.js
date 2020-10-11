3510b1f6fac01f247b14f7dc41852db13c856d56
Jeeyoon <acetaire@naver.com>
2020-10-11 01:00:05 -0400

    rollback and add youtube embed in cms

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import Figure from './Figure'
+import React from "react";
+import Figure from "./Figure";
+import getYouTubeId from "get-youtube-id";
+import YouTube from "react-youtube";
 
 const serializers = {
   types: {
-    authorReference: ({node}) => <span>{node.author.name}</span>,
-    mainImage: Figure
-  }
-}
+    authorReference: ({ node }) => <span>{node.author.name}</span>,
+    mainImage: Figure,
+    youtube: ({ node }) => {
+      const { url } = node;
+      const id = getYouTubeId(url);
 
-export default serializers
+      return <YouTube videoId={id} />;
+    },
+  },
+};
+
+export default serializers;
